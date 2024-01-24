ba1a39652352d60ec760eff3cbd2b3b8ee51d0f9
Patryk <74723494+PZ-webdev@users.noreply.github.com>
2024-01-24 18:19:45 +0100

    Implementation - Open Sky API (#4)
    * Fetch data and displaying on screen
    
    * Run fetchDataInBackground every 5 seconds

--- app/src/main/java/com/pzwebdev/skytrack/ui/component/AppNavigation.kt
+++ app/src/main/java/com/pzwebdev/skytrack/ui/component/AppNavigation.kt
@@ -4,14 +4,19 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.pzwebdev.skytrack.ui.screen.FlightDetailsScreen
 import com.pzwebdev.skytrack.ui.screen.FlightViewScreen
+import com.pzwebdev.skytrack.viewModel.FlightDataViewModel
 
 @Composable
-fun AppNavigation() {
+fun AppNavigation(flightDataViewModel: FlightDataViewModel) {
     val navController = rememberNavController()
 
     NavHost(navController = navController, startDestination = "flightViewScreen") {
-        composable("flightViewScreen") { FlightViewScreen(navController) }
-//        composable("mapScreen") { MapScreen(navController) }
+        composable("flightViewScreen") { FlightViewScreen(navController, flightDataViewModel) }
+        composable("flightDetailsScreen/{id}") { backStackEntry ->
+            val markerId = backStackEntry.arguments?.getString("id")
+            FlightDetailsScreen(markerId)
+        }
     }
 }
--- app/src/main/java/com/pzwebdev/skytrack/ui/screen/FlightViewScreen.kt
+++ app/src/main/java/com/pzwebdev/skytrack/ui/screen/FlightViewScreen.kt
@@ -1,31 +1,48 @@
 package com.pzwebdev.skytrack.ui.screen
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import android.util.Log
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
-import com.pzwebdev.skytrack.ui.component.GoogleMapView
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MapProperties
+import com.google.maps.android.compose.MapType
+import com.google.maps.android.compose.MapUiSettings
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.rememberCameraPositionState
+import com.pzwebdev.skytrack.viewModel.FlightDataViewModel
 
 @Composable
-fun FlightViewScreen(navController: NavController) {
-    Column(
+fun FlightViewScreen(
+    navController: NavController,
+    flightDataViewModel: FlightDataViewModel,
+) {
+    val uiSettings: MapUiSettings by remember { mutableStateOf(MapUiSettings()) }
+    val properties by remember { mutableStateOf(MapProperties(mapType = MapType.NORMAL)) }
+    val cameraPositionState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(LatLng(50.0295, 22.0067), 6f)
+    }
+
+    val flightDataList by flightDataViewModel.flightDataList.observeAsState(emptyList())
+
+    GoogleMap(
         modifier = Modifier
             .fillMaxSize(),
-        verticalArrangement = Arrangement.Top,
-        horizontalAlignment = Alignment.CenterHorizontally
+        properties = properties,
+        uiSettings = uiSettings,
+        cameraPositionState = cameraPositionState
     ) {
-        GoogleMapView()
+        flightDataList.forEach { flightData ->
+            Marker(position = LatLng(flightData.lat!!, flightData.lng!!)) {
+                // Dostosuj marker, dodaj opcje itp., jeśli to konieczne
+            }
+        }
     }
-}
-
-@Composable
-@Preview
-fun FlightViewScreenPreview() {
-    val navController = rememberNavController()
-    FlightViewScreen(navController = navController)
 }
